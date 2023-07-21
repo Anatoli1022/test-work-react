7f7d2cd045a3cfcfd3008bcdd85770bc86e09a26
Anatoli1022 <118132724+Anatoli1022@users.noreply.github.com>
2023-07-21 22:45:37 +0300

    fix: dataTable

--- src/components/dataTable/DataTable.js
+++ src/components/dataTable/DataTable.js
@@ -3,9 +3,7 @@ import classNames from 'classnames/bind';
 import axios from 'axios';
 import DataItem from '../dataItems/DataItem';
 import SearchBar from '../searchBar/SearchBar';
-
 import Pagination from '../pagination/Pagination';
-
 import styles from './DataTable.module.scss';
 
 const cx = classNames.bind(styles);
@@ -14,6 +12,11 @@ const DataTable = () => {
   const [data, setData] = useState([]);
   const [filteredData, setFilteredData] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
+  const [sortState, setSortState] = useState({
+    id: 'asc',
+    title: 'asc',
+    body: 'asc',
+  });
   const itemsPerPage = 10;
 
   useEffect(() => {
@@ -39,25 +42,54 @@ const DataTable = () => {
 
   const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
+  const applySort = (column) => {
+    setSortState((prevState) => ({
+      ...prevState,
+      [column]: prevState[column] === 'asc' ? 'desc' : 'asc',
+    }));
+
+    const sortedData = filteredData.slice().sort((a, b) => {
+      const sortOrder = sortState[column] === 'asc' ? 1 : -1;
+
+      if (column === 'id') return sortOrder * (a.id - b.id);
+      if (column === 'title')
+        return sortOrder * (a.title.length - b.title.length);
+      if (column === 'body') return sortOrder * (a.body.length - b.body.length);
+
+      return 0;
+    });
+
+    setFilteredData(sortedData);
+  };
+
   return (
     <div className={cx('container')}>
-    <div className={cx('wrapper')}>
-    <SearchBar data={data} setFilteredData={setFilteredData} />
-      <div className={cx('heading')}>
-        <span className={cx('text')}>id</span>
-        <p className={cx('text')}>Заголовок</p>
-        <p className={cx('text')}>Описание</p>
+      <div className={cx('wrapper')}>
+        <SearchBar data={data} setFilteredData={setFilteredData} />
+
+        <div className={cx('heading')}>
+          <button className={cx('button')} onClick={() => applySort('id')}>
+            id
+          </button>
+          <button className={cx('button')} onClick={() => applySort('title')}>
+            Заголовок
+          </button>
+          <button className={cx('button')} onClick={() => applySort('body')}>
+            Описание
+          </button>
+        </div>
+        <table>
+          {currentItems.map((item) => (
+            <DataItem key={item.id} item={item} />
+          ))}
+        </table>
+        <Pagination
+          itemsPerPage={itemsPerPage}
+          totalItems={filteredData.length}
+          paginate={paginate}
+          currentPage={currentPage}
+        />
       </div>
-      {currentItems.map((item) => (
-        <DataItem key={item.id} item={item} />
-      ))}
-      <Pagination
-        itemsPerPage={itemsPerPage}
-        totalItems={filteredData.length}
-        paginate={paginate}
-        currentPage={currentPage}
-      />
-    </div>
     </div>
   );
 };
