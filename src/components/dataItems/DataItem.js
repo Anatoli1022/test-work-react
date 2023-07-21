0201978f8291a4450697950833bcf9e1c1510a27
Anatoli1022 <118132724+Anatoli1022@users.noreply.github.com>
2023-07-21 22:21:47 +0300

    fix: data item

--- src/components/dataItems/DataItem.js
+++ src/components/dataItems/DataItem.js
@@ -6,21 +6,11 @@ const cx = classNames.bind(styles);
 
 const DataItem = ({ item }) => {
   return (
-    <div className={cx('wrapper')}>
-      <ul className={cx('information')}>
-        <li className={cx('item')}>
-          <span className={cx('info-text', 'id')}>{item.id}</span>
-        </li>
-
-        <li className={cx('item')}>
-          <h5 className={cx('info-text', 'title')}>{item.title}</h5>
-        </li>
-
-        <li className={cx('item')}>
-          <p className={cx('info-text', 'text')}>{item.body}</p>
-        </li>
-      </ul>
-    </div>
+    <tr className={cx('information')}>
+      <th className={cx('item')}> {item.id}</th>
+      <th className={cx('item')}>{item.title}</th>
+      <th className={cx('item')}> {item.body}</th>
+    </tr>
   );
 };
 
